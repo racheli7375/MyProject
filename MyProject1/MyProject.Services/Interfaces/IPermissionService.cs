4a10890b4bc7058c84d72460a2c56159086bf6b3
racheli7375 <hitmachut@students.local>
2022-11-20 16:57:55 +0200

    first commit

---
+++ MyProject1/MyProject.Services/Interfaces/IPermissionService.cs
@@ -0,0 +1,25 @@
+﻿using MyPeoject.Common;
+using MyProject.Repositories.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyProject.Services.Interfaces
+{
+   
+    public interface IPermissionService
+    {
+        List<PermissionDTO> GetAll();
+
+        PermissionDTO GetById(int id);
+
+        PermissionDTO Add(int id, string name, string description);
+
+        PermissionDTO Update(Permission Permission);
+
+        void Delete(int id);
+
+    }
+}
